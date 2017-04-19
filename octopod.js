7704873bbff95a25b78c0d73fff90976345d7a78
Thibaut Gery <thibaut.gery@gmail.com>
2017-04-19 15:09:17 +1000

    get Id from octopod

--- octopod.js
+++ octopod.js
@@ -3,9 +3,11 @@ const rp = require('request-promise-native')
 function sync(event, context, callback) {
   const clientId = process.env.CLIENT_ID
   const clientSecret = process.env.CLIENT_SECRET
+  const trigram = event.path.id
 
   getAuth(clientId, clientSecret)
-    .then(accessToken => callback(null, { "access": accessToken} ))
+    .then(accessToken => getOctoId(trigram, accessToken))
+    .then(id => callback(null, { "id": id} ))
     .catch(callback)
 }
 
@@ -20,7 +22,24 @@ function getAuth(clientId, clientSecret) {
         client_secret: clientSecret
     }
   };
-  return rp(options).then((body) => "Authorization: Bearer " + JSON.parse(body).access_token)
+  return rp(options).then((body) => "Bearer " + JSON.parse(body).access_token)
 }
 
-module.exports = {sync, getAuth}
+function getOctoId(trigram, authToken) {
+  var options = {
+    method: 'GET',
+    uri: 'https://octopod.octo.com/api/v0/people?all=true',
+    headers: {
+        Authorization: authToken
+    }
+  };
+  return rp(options)
+    .then(body => {
+      body = JSON.parse(body)
+      const personFoundAsArray = body.filter(i => i.nickname === trigram)
+      if(personFoundAsArray.length === 0) throw new Error(`Person ${trigram} not found`)
+      return body.filter(i => i.nickname === trigram)[0].id
+    })
+}
+
+module.exports = {sync, getAuth, getOctoId}
--- tests/octopod.js
+++ tests/octopod.js
@@ -3,10 +3,63 @@ const nock = require('nock')
 const octopod = require("../octopod")
 
 const octopodCall = nock('https://octopod.octo.com')
+const octopodCallWithAuth = nock('https://octopod.octo.com', {
+      reqheaders: {Authorization: "Bearer toto"}})
 
-describe("Octopod: ", () => {
+const people = [
+  {
+    "id": 2142664765,
+    "last_name": "Lamrani",
+    "first_name": "El Khadir",
+    "nickname": "EKL",
+    "url": "https://octopod.octo.com/api/v0/people/2142664765",
+    "email": "eklamrani@octo.com",
+    "job": {
+      "id": 49,
+      "name": "Consultant",
+      "url": "https://octopod.octo.com/api/v0/jobs/49"
+    },
+    "lob": {
+      "id": 9,
+      "abbreviation": "MA",
+      "url": "https://octopod.octo.com/api/v0/lobs/9"
+    },
+    "included_in_activity_rate": true,
+    "created_at": "2017-04-18T07:18:30Z",
+    "updated_at": "2017-04-18T07:18:30Z",
+    "entry_date": "2017-04-17",
+    "leaving_date": null
+  },
+  {
+    "id": 2142664764,
+    "last_name": "Noël-Leroux",
+    "first_name": "Philippe",
+    "nickname": "PNL",
+    "url": "https://octopod.octo.com/api/v0/people/2142664764",
+    "email": "pleroux@octo.com",
+    "job": {
+      "id": 58,
+      "name": "Manager Biz",
+      "url": "https://octopod.octo.com/api/v0/jobs/58"
+    },
+    "lob": {
+      "id": 80,
+      "abbreviation": "GBIZ",
+      "url": "https://octopod.octo.com/api/v0/lobs/80"
+    },
+    "included_in_activity_rate": true,
+    "created_at": "2017-04-10T13:01:21Z",
+    "updated_at": "2017-04-18T11:38:27Z",
+    "entry_date": "2017-05-02",
+    "leaving_date": null
+  }
+]
+
+describe("Octopod'integration: ", () => {
   describe("when getting the token", () => {
-    it("return the  token", (done) => {
+    it("return the  token", () => {
+
+      //given
       const authCall = octopodCall
                         .post('/api/oauth/token',
                         "grant_type=client_credentials&client_id=client_credentials&client_secret=client_secret")
@@ -16,12 +69,53 @@ describe("Octopod: ", () => {
                           expires_in: 7200,
                           created_at: 1492567576
                         });
+
+      //when
       const auth = octopod.getAuth('client_credentials', 'client_secret');
-      auth.then((token) => {
+
+      //then
+      return auth.then((token) => {
         authCall.isDone();
-        expect(token).to.equal("Authorization: Bearer 4f44c9a8b8fe900583d711c5fb6a23f0766bf4b7214425b098d4f7319977445")
-        done();
-      }, done)
+        expect(token).to.equal("Bearer 4f44c9a8b8fe900583d711c5fb6a23f0766bf4b7214425b098d4f7319977445")
+      })
     })
+  })
+
+  describe("when getting the trigrams", () => {
+    describe("when the trigram exist", () => {
+      it("return the  token", () => {
+        //given
+        const idCall = octopodCallWithAuth
+                          .get('/api/v0/people?all=true')
+                          .reply(200, people);
+
+        //when
+        const idP = octopod.getOctoId('EKL', 'Bearer toto');
+
+        //then
+        return idP.then((id) => {
+          idCall.isDone();
+          expect(id).to.equal(2142664765)
+        })
+      })
+    })
+
+    describe("when the trigram doesn't exist", () => {
+      it("return the  token", (done) => {
+        //givenconst
+        idCall = octopodCallWithAuth
+                  .get('/api/v0/people?all=true')
+                  .reply(200, people);
+        //when
+        const idP = octopod.getOctoId('TGE', 'Bearer toto');
+
+        //then
+        idP.then(done).catch(err => {
+          expect(err.message).to.equal("Person TGE not found")
+          done()
+        })
+      })
+    })
+
   })
 })
