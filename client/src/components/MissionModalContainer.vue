259a2686bd4f81027949c75af1793ab862d8af66
Favre Eric <efavre@octo.com>
2017-05-15 22:59:45 +1000

    cleaner tag and description management

--- client/src/components/MissionModalContainer.vue
+++ client/src/components/MissionModalContainer.vue
@@ -11,8 +11,8 @@
           </div>
           <div class="mission-desc">
             <h2><input v-model="mission.role"> for <input v-model="mission.customer"></h2>
-            <textarea v-model="missionDescription" placeholder="- ...\n- ..." rows="5" cols="100"></textarea>
-            <p class="mission-keywords"><input v-model.lazy="missionTags"></p>
+            <textarea v-model="missionDescription" rows="5" cols="100"></textarea>
+            <p class="mission-keywords"><input placeholder="tag1, tag2" v-model.lazy="missionTags"></p>
           </div>
           <button class="modal-default-button" v-on:click="updateMission">OK</button>
         </div>
@@ -32,7 +32,7 @@ export default {
       trigram: '',
       mission: {},
       missionTags: '',
-      missionDescription: ''
+      missionDescription: '-... \n-...'
     }
   },
   mounted () {
@@ -52,8 +52,12 @@ export default {
     set (mission, trigram) {
       this.trigram = trigram
       this.mission = mission
-      this.missionTags = mission.tags ? mission.tags.join(',') : ''
-      this.missionDescription = '- ' + (mission.description ? mission.description.join('\n- ') : '...')
+      if (mission.tags) {
+        this.missionTags = mission.tags.join(',')
+      }
+      if (mission.description) {
+        this.missionDescription = '- ' + mission.description.join('\n- ')
+      }
     },
     updateMission () {
       this.mission.tags = this.missionTags.split(',')
