e799857198d853b44a4431e1b2997ec559799463
Favre Eric <efavre@octo.com>
2017-05-16 13:15:59 +1000

    better multiline string - json array mapping

--- client/src/components/ProfileModalContainer.vue
+++ client/src/components/ProfileModalContainer.vue
@@ -30,6 +30,11 @@
             <h2>Publications / Responsibilities / Certifications</h2>
              <textarea v-model="achievementSkills" placeholder="- ..." rows="5" cols="50"></textarea>
           </div>
+          <div class="divider"></div>
+          <div class="bloc bloc-skills">
+            <h2>Others</h2>
+            <textarea v-model="otherSkills" placeholder="- ..." rows="5" cols="50"></textarea>
+          </div>
           <button class="modal-default-button" v-on:click="updateProfile">OK</button>
         </div>
       </div>
@@ -50,6 +55,7 @@ export default {
       architectureTechnologySkills: '',
       methodologySkills: '',
       achievementSkills: '',
+      otherSkills: '',
       trigram: ''
     }
   },
@@ -69,19 +75,21 @@ export default {
     },
     set (profile, trigram) {
       this.profile = profile
-      this.education = '- ' + profile.education.join('\n- ')
-      this.technicalSkills = '- ' + profile.skills.technical.join('\n- ')
-      this.architectureTechnologySkills = '- ' + profile.skills.architectureTechnologies.join('\n- ')
-      this.methodologySkills = '- ' + profile.skills.methodologies.join('\n- ')
-      this.achievementSkills = '- ' + profile.skills.achievements.join('\n- ')
+      this.education = this.mapArrayToMultilineList(profile.education)
+      this.technicalSkills = this.mapArrayToMultilineList(profile.skills.technical)
+      this.architectureTechnologySkills = this.mapArrayToMultilineList(profile.skills.architectureTechnologies)
+      this.methodologySkills = this.mapArrayToMultilineList(profile.skills.methodologies)
+      this.achievementSkills = this.mapArrayToMultilineList(profile.skills.achievements)
+      this.otherSkills = this.mapArrayToMultilineList(profile.skills.others)
       this.trigram = trigram
     },
     updateProfile () {
-      this.profile.education = this.education.substring(2).split('\n- ')
-      this.profile.skills.technical = this.technicalSkills.substring(2).split('\n- ')
-      this.profile.skills.architectureTechnologies = this.architectureTechnologySkills.substring(2).split('\n- ')
-      this.profile.skills.methodologies = this.methodologySkills.substring(2).split('\n- ')
-      this.profile.skills.achievements = this.achievementSkills.substring(2).split('\n- ')
+      this.profile.education = this.mapMultilineListToArray(this.education)
+      this.profile.skills.technical = this.mapMultilineListToArray(this.technicalSkills)
+      this.profile.skills.architectureTechnologies = this.mapMultilineListToArray(this.architectureTechnologySkills)
+      this.profile.skills.methodologies = this.mapMultilineListToArray(this.methodologySkills)
+      this.profile.skills.achievements = this.mapMultilineListToArray(this.achievementSkills)
+      this.profile.skills.others = this.mapMultilineListToArray(this.otherSkills)
       return axios.put(process.env.API_URL + process.env.UPDATE_BASICS_PATH.replace('{trigram}', this.trigram))
         .then((response) => {
           this.profile = response.data
@@ -91,6 +99,18 @@ export default {
           console.log(e)
           this.errors.push(e)
         })
+    },
+    mapArrayToMultilineList (jsonArray) {
+      if (jsonArray && jsonArray.length > 0) {
+        return '- ' + jsonArray.join('\n- ')
+      }
+      return ''
+    },
+    mapMultilineListToArray (multilineList) {
+      if (multilineList && multilineList.startsWith('- ')) {
+        return multilineList.substring(2).split('\n- ')
+      }
+      return []
     }
   }
 }
